983cb0c5ada4f02b81a178d4a7ecbf50038dec6d
Victor Skits <vityok.nice@gmail.com>
2024-06-23 12:18:59 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,17 @@
+"use strict";
+const container = document.querySelector(".tree");
+container.addEventListener("click", (e)=>{
+    const list = e.target.nextSibling;
+    if (list) list.hidden = !list.hidden;
+});
+function wrapWithSpan() {
+    const listItems = container.querySelectorAll("li");
+    listItems.forEach((item)=>{
+        const wrapper = document.createElement("span");
+        item.prepend(wrapper);
+        wrapper.append(wrapper.nextSibling);
+    });
+}
+wrapWithSpan();
+
+//# sourceMappingURL=index.f75de5e1.js.map
